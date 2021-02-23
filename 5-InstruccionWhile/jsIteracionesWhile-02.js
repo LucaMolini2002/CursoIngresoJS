d9bfca0d5f622e535536a9bdc6bdb7cc7dfdea77
Luca <luca.molinist@gmail.com>
2021-02-22 23:04:25 -0300

    Nuevo cambio

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,11 +3,10 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var numero=10;
 	var acumulador=0;
-	var contador=0;
-	while(contador>0){
-		acumulador=contador+numero;
+	var contador=10;
+	while(contador>=1){
+		acumulador=contador;
 		contador=contador-1;
 		alert(acumulador);
 	}
